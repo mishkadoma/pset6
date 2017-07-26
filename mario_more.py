abfc9c140efec443f80a0612a5982c2f1eea03a1
Mickhail <mixandoma@ya.ru>
2017-07-26 15:45:27 +0000

    double piramide

---
+++ mario_more.py
@@ -0,0 +1,12 @@
+while True:
+    height = int(input('Enter the piramide height > '))
+    if height >= 0:
+        break
+
+for i in range(0, height):
+
+    indentation =  height - (i + 1)
+    print(" " * indentation, end = "")
+    print("#" * (i + 2), end = "")
+    print("#" * (i + 2), end = "")
+    print(" " * indentation)
