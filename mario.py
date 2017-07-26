bec71505072637e5bedb650029164039654946ba
Mickhail <mixandoma@ya.ru>
2017-07-26 15:29:38 +0000

    custom_validation added

--- mario.py
+++ mario.py
@@ -1,7 +1,9 @@
-height = int(input('Enter the piramide height > '))
+while true:
+    height = int(input('Enter the piramide height > '))
+    if height >= 0:
+        break
 
 for i in range(0, height):
-
     indentation =  height - (i + 1)
     print(" " * indentation, end = "")
     print("#" * (i + 2))
